ab7199354df8432b39af06bc338c17c793843066
Devulapally-MaheshBabu <devulapallymaahi1@gmail.com>
2026-03-30 14:19:00 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://heart-disease-risk-prediction-w58h.onrender.com";
 
 const FIELDS = [
   {
